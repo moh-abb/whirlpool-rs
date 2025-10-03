f2a94b62973093e70bd9a7fc8e75f172d8531cbd
Mohammad A <ma3523@imperial.ac.uk>
2025-10-03 12:09:09 +0100

    refactor: Converts package to library.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! A program for creating Strudel live-coded music, specialised for embedded
+//! systems with limited memory allocation.
+
+#![cfg_attr(not(any(doc, feature = "std", test)), no_std)]
+#![allow(clippy::let_and_return)]
+#![deny(missing_docs, clippy::undocumented_unsafe_blocks, dead_code)]
