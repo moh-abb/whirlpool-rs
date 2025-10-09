825fe39ea69a62bdffdba584007721cb3bf0c4dd
Mohammad A <ma3523@imperial.ac.uk>
2025-10-09 18:24:07 +0100

    feat: Creates struct `Number`.

--- src/ast/note.rs
+++ src/ast/note.rs
@@ -1,6 +1,10 @@
 #[cfg(test)]
 use proptest_derive::Arbitrary;
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub struct Number(pub u16);
+
 #[allow(unused)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 #[cfg_attr(test, derive(Arbitrary))]
