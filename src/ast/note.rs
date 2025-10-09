d1f8917aeb935445837327d048a200d0505899fe
Mohammad A <ma3523@imperial.ac.uk>
2025-10-09 18:23:31 +0100

    feat: Creates enum `Letter`.

--- src/ast/note.rs
+++ src/ast/note.rs
@@ -1,6 +1,24 @@
 #[cfg(test)]
 use proptest_derive::Arbitrary;
 
+#[allow(unused)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[cfg_attr(test, derive(Arbitrary))]
+pub enum Letter {
+    A,
+    ASharp,
+    B,
+    C,
+    CSharp,
+    D,
+    DSharp,
+    E,
+    F,
+    FSharp,
+    G,
+    GSharp,
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct Frequency(pub f32);
