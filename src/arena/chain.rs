e7faecd830affe203f03fcdfab5d2543750677e1
Mohammad A <ma3523@imperial.ac.uk>
2025-12-18 01:57:28 +0000

    fix: Implements chain cloning for all types.

--- src/arena/chain.rs
+++ src/arena/chain.rs
@@ -1,12 +1,23 @@
 use super::index::Index;
 
 #[allow(unused)]
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 /// A type used to create a singly-linked arena-allocated list of items of type
 /// `T`, where the `next` pointer is an [Index] to the same [Chain] type.
 pub enum Chain<T> {
     Cons { head: Index<T>, tail: Index<Self> },
     Nil,
+}
+
+impl<T> Clone for Chain<T> {
+    fn clone(&self) -> Self {
+        match self {
+            Self::Cons { head, tail } => {
+                Self::Cons { head: head.clone(), tail: tail.clone() }
+            }
+            Self::Nil => Self::Nil,
+        }
+    }
 }
 
 #[derive(Debug)]
