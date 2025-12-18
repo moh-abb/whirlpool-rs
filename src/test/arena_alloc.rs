fc0547124b6bc7b7519c5bda71e7dbd9e85e0d13
Mohammad A <ma3523@imperial.ac.uk>
2025-12-18 02:43:41 +0000

    refactor: Separates out construction of growable arena tuple in `arena_alloc`.

--- src/test/arena_alloc.rs
+++ src/test/arena_alloc.rs
@@ -1,3 +1,5 @@
+use core::error::Error;
+
 use proptest::prelude::TestCaseError;
 use proptest::test_runner::TestRunner;
 
@@ -7,31 +9,39 @@ use crate::arena::error::ArenaError;
 use crate::arena::tuple::ArenaTuple;
 use crate::ast::pattern::Pattern;
 use crate::ast::pattern::TimedStep;
+use crate::test::arbitrary::DynArenasOf;
 use crate::test::arbitrary::arb_pattern;
 
+fn with_growable_arena_tuple<U>(
+    f: impl FnOnce(DynArenasOf<'_, Pattern>) -> U,
+) -> U {
+    let pattern_arena = GrowableArena::<Pattern>::new();
+    let chain_arena = GrowableArena::<Chain<Pattern>>::new();
+    let timed_step_arena = GrowableArena::<TimedStep>::new();
+    let timed_step_chain_arena = GrowableArena::<Chain<TimedStep>>::new();
+    let arena_tuple = (
+        pattern_arena,
+        (chain_arena, (timed_step_arena, (timed_step_chain_arena, ()))),
+    );
+    let dyn_arenas = ArenaTuple::to_dyn_arenas(&arena_tuple);
+    f(dyn_arenas)
+}
+
 #[test]
-fn allocation_in_growable_arenas_should_not_fail() {
+fn can_allocate_multiple_in_growable_arenas() -> Result<(), impl Error> {
     let mut test_runner = TestRunner::deterministic();
     let strat = arb_pattern();
-    let test_run_result = test_runner.run(&strat, |pat| {
-        let pattern_arena = GrowableArena::<Pattern>::new();
-        let chain_arena = GrowableArena::<Chain<Pattern>>::new();
-        let timed_step_arena = GrowableArena::<TimedStep>::new();
-        let timed_step_chain_arena = GrowableArena::<Chain<TimedStep>>::new();
-        let arena_tuple = (
-            pattern_arena,
-            (chain_arena, (timed_step_arena, (timed_step_chain_arena, ()))),
-        );
-        let generated_pattern = (pat.0)(arena_tuple.to_dyn_arenas());
-        let error_message = |e: ArenaError| {
-            format!(
-                "Generating pattern with arena tuple {arena_tuple:?}
-                 failed and gave {e:?}"
-            )
-        };
-        generated_pattern
-            .map(|_| ())
-            .map_err(|e| TestCaseError::fail(error_message(e)))
-    });
-    test_run_result.unwrap()
+    with_growable_arena_tuple(|arena_tuple| {
+        test_runner.run(&strat, |pat| {
+            let generated_pattern = (pat.0)(arena_tuple);
+            let error_message = |e: ArenaError| {
+                format!(
+                    "Generating pattern with arena tuple failed and gave {e:?}"
+                )
+            };
+            generated_pattern
+                .map(|_| ())
+                .map_err(|e| TestCaseError::fail(error_message(e)))
+        })
+    })
 }
