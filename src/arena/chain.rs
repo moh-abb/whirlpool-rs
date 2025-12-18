cb97fad005cd3ea8ead90efe93d1666bebdda998
Mohammad A <ma3523@imperial.ac.uk>
2025-12-18 00:43:12 +0000

    feat: Derives comparison and cloning on `Chain`.

--- src/arena/chain.rs
+++ src/arena/chain.rs
@@ -1,7 +1,7 @@
 use super::index::Index;
 
 #[allow(unused)]
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 /// A type used to create a singly-linked arena-allocated list of items of type
 /// `T`, where the `next` pointer is an [Index] to the same [Chain] type.
 pub enum Chain<T> {
