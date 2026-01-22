752f2f6f5cded9ffb6fae66616fca4671e93e9b0
Mohammad A <ma3523@imperial.ac.uk>
2026-01-22 18:49:46 +0000

    feat: Adds helper constants to `CycleTime`.

--- src/ast/time/mod.rs
+++ src/ast/time/mod.rs
@@ -5,6 +5,11 @@ use fixed::types::extra::U12;
 /// Used to represent cycles, units of time (progression through a
 /// [crate::ast::pattern::Pattern]) that are not dependent on the number of
 /// beats per minute.
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-#[allow(unused)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct CycleTime(pub Wrapping<FixedU32<U12>>);
+
+#[allow(unused)]
+impl CycleTime {
+    pub const ZERO: Self = Self(Wrapping(FixedU32::ZERO));
+    pub const ONE: Self = Self(Wrapping(FixedU32::const_from_int(1)));
+}
