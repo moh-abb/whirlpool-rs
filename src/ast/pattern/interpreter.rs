7899643b6d9f009c55a407f4091e06a1105ac6aa
Mohammad A <ma3523@imperial.ac.uk>
2026-02-07 03:59:30 +0000

    feat: Reimplements interpreter for `Cat` case using an iterator of contiguous segments of intervals.

--- src/ast/pattern/interpreter.rs
+++ src/ast/pattern/interpreter.rs
@@ -3,10 +3,6 @@ use core::iter;
 use core::marker::PhantomData;
 use core::ops::DerefMut;
 
-use fixed::Wrapping;
-
-use crate::structures::index::Index;
-use crate::structures::multiple::Multiple;
 use crate::ast::pattern::Pattern;
 use crate::ast::pattern::arenas::PatternArenas;
 use crate::ast::pattern::note::NoteUnit;
@@ -16,6 +12,8 @@ use crate::ast::time::CycleTime;
 use crate::ast::time::CycleTimeInterval;
 use crate::player::PatternPlayer;
 use crate::player::unit::SoundUnit;
+use crate::structures::index::Index;
+use crate::structures::multiple::Multiple;
 
 mod private {
     use core::cell::RefCell;
@@ -38,6 +36,9 @@ mod private {
     }
 }
 
+/// An interpreter of [Pattern]s, which keeps track of a current time and
+/// plays units (traversing the [Pattern]'s tree) when new units are
+/// encountered.
 #[allow(unused)]
 pub struct Interpreter<'a, Arenas, Player, B: private::BorrowAdapter<Player>> {
     pattern: Index<Pattern>,
@@ -99,9 +100,10 @@ impl<
         let mut borrowed_player = self.borrow_adapter.borrow_mut();
         let visitor = InterpreterVisitor {
             arenas: self.arenas,
-            interval: CycleTimeInterval::new(self.position, next_position),
+            start: self.position,
+            duration: next_position.sub(self.position),
             offset: CycleTime::ZERO,
-            multiplier: 1,
+            multiplier: CycleTime::ONE,
             inner: RefCell::new(VisitorInner {
                 player: borrowed_player.deref_mut(),
             }),
@@ -111,129 +113,63 @@ impl<
     }
 }
 
+/// A visitor used to traverse a given [Pattern].
+/// - `arenas` is a reference to the arenas where a given [Pattern] is stored.
+/// - `interval` is the cycle time (start inclusive, end exclusive) to play
+///   any given units.
+/// - `multiplier` is used to scale down the duration of notes; i.e., play
+///   units faster.
+/// - `offset` is used to add an offset to the start time of units played.
+/// - `inner` contains the `Player` from which units will be scheduled.
 struct InterpreterVisitor<'a, Arenas, Player> {
     arenas: &'a Arenas,
-    interval: CycleTimeInterval,
-    multiplier: u32,
+    start: CycleTime,
+    duration: CycleTime,
     offset: CycleTime,
+    multiplier: CycleTime,
     inner: RefCell<VisitorInner<'a, Player>>,
 }
 
-trait PatternConsumer<T> {
-    fn consume(&self, f: impl FnMut(T, CycleTime));
-}
-
-struct CatConsumer<'a, P>(Multiple<Pattern>, &'a P);
-
-impl<P: PatternArenas> PatternConsumer<Index<Pattern>> for CatConsumer<'_, P> {
-    fn consume(&self, mut f: impl FnMut(Index<Pattern>, CycleTime)) {
-        let Self(multiple, arenas) = self;
-        let chain_arena = arenas.get_pattern_chain_arena();
-        multiple.fold_left(chain_arena, (), |(), pattern_index| {
-            f(pattern_index, CycleTime::ONE)
-        })
-    }
-}
-
-struct NoteConsumer(NoteUnit);
-
-impl PatternConsumer<NoteUnit> for NoteConsumer {
-    fn consume(&self, mut f: impl FnMut(NoteUnit, CycleTime)) {
-        f(self.0, CycleTime::ONE)
-    }
-}
-
-/// Plays patterns in `interval` from a given `consumer`:
-/// The consumer takes a function that processes subpatterns with their length,
-/// and applies it to any patterns stored inside the subpattern.
+/// Splits up a given time (denoted by start time and duration) by a given
+/// cycle duration and increment. Returns intervals of duration less than or
+/// equal to the increment, with the first returned interval aligning to the
+/// cycle length. If one returned interval is not included in the total
+/// interval, then the interval is [Option::None].
 ///
-/// The cycle length is determined by the total length (integer cycles) of the
-/// consumer. For a singleton [Pattern::Note] or [Pattern::Seq], this is one,
-/// for [Pattern::Cat], this is the [Multiple]'s length, and so on.
-fn play_consumer<T, F: FnMut(T, CycleTimeInterval, CycleTime)>(
-    consumer: impl PatternConsumer<T>,
+/// If the start (or end) time is not aligned to the increment, then the
+/// first (or last) interval will have a duration less than the cycle duration.
+fn interval_chunks_with_increments(
     interval: CycleTimeInterval,
-    cycle_length: i32,
-    mut play_subpattern: F,
-) {
-    // We assume the repetition occurs at multiples of CycleTime(cycle_length).
-    let fixed_cycle_length = Wrapping::from_num(cycle_length);
+    cycle_length: CycleTime,
+    increment: CycleTime,
+) -> impl Iterator<Item = Option<CycleTimeInterval>> {
+    let start = interval.start();
+    let end = interval.end();
+    let aligned_start = start.round_down_to_nearest(cycle_length);
+    let mut window_start = aligned_start;
+    let windows = iter::repeat_with(move || {
+        let window_end = window_start.add(increment);
+        let window = CycleTimeInterval::new(window_start, window_end);
+        window_start = window_end;
+        window
+    });
+    windows
+        .take_while(move |window| window.start() < end)
+        .map(move |window| window.intersection(interval))
+}
 
-    // Because we may currently be partway through a cycle, we need to start
-    // at the beginning of the cycle to be sure.
-    let mut init_cycle_start = interval.start().0;
-    let time_from_start_of_cycle =
-        init_cycle_start.rem_euclid_int(cycle_length);
-    init_cycle_start -= time_from_start_of_cycle;
-    let init_multiple_repetitions = init_cycle_start
-        .div_euclid_int(cycle_length)
-        .to_num::<usize>();
-
-    let multiple_starts_and_ends = iter::repeat(())
-        .scan(CycleTime(init_cycle_start), |state, ()| {
-            let cur_start = *state;
-            *state = CycleTime(cur_start.0 + fixed_cycle_length);
-            Some(cur_start)
-        })
-        .take_while(|start| start < &interval.end())
-        .enumerate()
-        .map(|(i, x)| (i + init_multiple_repetitions, x));
-
-    for (multiple_repetitions, multiple_start) in multiple_starts_and_ends {
-        // Choose the pattern(s) to play by performing a modulo on the
-        // length of the pattern.
-        // If cur_repetition_int = k, then this means that the subpatterns
-        // will be played at times k to k + 1.
-
-        // Play each element inside the repetition,
-        // ensuring that there is an intersection between the supposed
-        // scheduled start/end time, and the visitor's start/end time.
-        // times of the
-        let mut scheduled_start = multiple_start;
-        let visiting_start = Wrapping::from_num(multiple_repetitions);
-
-        // For each item (such as a subpattern or note unit),
-        // play it from `visiting_start` to `visiting_start + duration`,
-        // such that the scheduled note units occur from `scheduled_start` to
-        // `scheduled_start + duration`.
-        let fold_item = |item: T, length: CycleTime| {
-            // Check that the subpattern is within the interval required.
-
-            // We have the interesting case that the subpattern being played
-            // will not always follow the same time as the outer pattern:
-            // We want to use the `visiting_start` to obtain
-            // the unit notes, but the `multiple_start` to schedule them.
-            // Hence, we need to use an offset variable, for which the
-            // callee will use to reconstuct the scheduled time.
-            let visiting_end = visiting_start + length.0;
-            let visiting_interval = CycleTimeInterval::new(
-                CycleTime(visiting_start),
-                CycleTime(visiting_end),
-            );
-            let scheduled_end = CycleTime(scheduled_start.0 + length.0);
-            let scheduled_interval =
-                CycleTimeInterval::new(scheduled_start, scheduled_end);
-
-            let should_play_subpattern = scheduled_interval
-                .intersection(interval)
-                .is_some();
-
-            if should_play_subpattern {
-                // Calculate the times for the subpatterns
-                let subpattern_offset = scheduled_start.0 - visiting_start;
-                play_subpattern(
-                    item,
-                    visiting_interval,
-                    CycleTime(subpattern_offset),
-                );
-            }
-
-            // Update the start times for the next subpattern.
-            scheduled_start = scheduled_end;
-        };
-
-        consumer.consume(fold_item);
-    }
+/// Splits up a given time (denoted by start time and duration) by a given
+/// cycle duration, into separate intervals, each with a start time and
+/// duration, such that each interval has length less than or equal to the
+/// cycle duration.
+/// If the start (or end) time is not aligned to the cycle duration, then the
+/// first (or last) interval will have a duration less than the cycle duration.
+fn interval_chunks(
+    interval: CycleTimeInterval,
+    cycle_length: CycleTime,
+) -> impl Iterator<Item = CycleTimeInterval> {
+    interval_chunks_with_increments(interval, cycle_length, cycle_length)
+        .flatten()
 }
 
 struct VisitorInner<'a, Player> {
@@ -273,27 +209,49 @@ impl<'a, Arenas: PatternArenas, Player: PatternPlayer> PatternVisitor
             panic!("Cannot play empty multiple patterns");
         }
 
-        let cycle_length = i32::from(multiple.length());
+        let cycle_length = CycleTime::from_int(i32::from(multiple.length()));
+
+        // Use the repetition count to calculate subpatterns' offsets.
         let play_subpattern =
-            |subpattern, visiting_interval, subpattern_offset: CycleTime| {
-                let mut inner_mut = self.inner.borrow_mut();
-                let subpattern_visitor = InterpreterVisitor {
-                    arenas: self.arenas,
-                    interval: visiting_interval,
-                    offset: CycleTime(self.offset.0 + subpattern_offset.0),
-                    multiplier: self.multiplier,
-                    inner: RefCell::new(VisitorInner {
-                        player: inner_mut.player,
-                    }),
-                };
-                visit_pattern(&subpattern_visitor, subpattern);
+            |unit_offset: CycleTime,
+             (opt_unit, pattern): (Option<CycleTimeInterval>, _)| {
+                if let Some(unit_interval) = opt_unit {
+                    let mut inner_mut = self.inner.borrow_mut();
+                    let subpattern_visitor = InterpreterVisitor {
+                        arenas: self.arenas,
+                        start: unit_interval.start().add(unit_offset),
+                        duration: CycleTime::ONE,
+                        offset: self.offset.sub(unit_offset),
+                        multiplier: self.multiplier,
+                        inner: RefCell::new(VisitorInner {
+                            player: inner_mut.player,
+                        }),
+                    };
+                    visit_pattern(&subpattern_visitor, pattern);
+                }
+                unit_offset.add(CycleTime::ONE)
             };
-        play_consumer(
-            CatConsumer(multiple, self.arenas),
-            self.interval,
-            cycle_length,
-            play_subpattern,
-        );
+
+        // Find out which repetition we are currently on; i.e., how many cycle
+        // lengths have elapsed until the start.
+        let start_repetition_count = self.start.div_euclid(cycle_length);
+        let play_repetition =
+            |repetition_count: CycleTime, repetition: CycleTimeInterval| {
+                let units = interval_chunks_with_increments(
+                    repetition,
+                    cycle_length,
+                    CycleTime::ONE,
+                );
+                units
+                    .zip(multiple.iter(self.arenas.get_pattern_chain_arena()))
+                    .fold(repetition_count, play_subpattern);
+                repetition_count.add(CycleTime::ONE)
+            };
+
+        let interval =
+            CycleTimeInterval::new(self.start, self.start.add(self.duration));
+        interval_chunks(interval, cycle_length)
+            .fold(start_repetition_count, play_repetition);
     }
 
     fn map_seq(&self, _multiple: Multiple<Pattern>) -> Self::PatternOutput {
@@ -321,34 +279,29 @@ impl<'a, Arenas: PatternArenas, Player: PatternPlayer> PatternVisitor
     }
 
     fn map_note_unit(&self, unit: NoteUnit) -> Self::PatternOutput {
-        let fixed_multiplier = Wrapping::from_num(self.multiplier);
-        let unit_duration = CycleTime(fixed_multiplier.recip());
-        let sound_unit = SoundUnit::new(unit, unit_duration);
+        let unit_duration = self.multiplier.recip();
+        let unit = SoundUnit::new(unit, unit_duration);
 
-        let play_subpattern =
-            |_unit, visiting_interval: CycleTimeInterval, offset: CycleTime| {
-                let mut inner = self.inner.borrow_mut();
-                let player = &mut inner.player;
+        let start = self.start;
+        let end = start.add(self.duration);
+        let interval = CycleTimeInterval::new(start, end);
 
-                debug_assert_eq!(_unit, unit);
-                let to_num = |cycle_time: CycleTime| {
-                    cycle_time.0.ceil().to_num::<u32>() / self.multiplier
-                };
-                let start = to_num(visiting_interval.start());
-                let end = to_num(visiting_interval.end());
+        // Only play notes which are aligned to the cycle (i.e., discard
+        // windows of size <1 which start midway through a unit).
+        let is_aligned_to_single_cycle = |interval: &CycleTimeInterval| {
+            let start = interval.start();
+            start == start.ceil()
+        };
+        let windows = interval_chunks(interval, CycleTime::ONE)
+            .filter(is_aligned_to_single_cycle);
 
-                for int_time in start..end {
-                    let fixed_time = Wrapping::from_num(int_time);
-                    let scheduled_start_time =
-                        CycleTime(fixed_time + self.offset.0 + offset.0);
+        let mut inner = self.inner.borrow_mut();
+        let player = &mut inner.player;
 
-                    player.schedule_note_unit(
-                        sound_unit.clone(),
-                        scheduled_start_time,
-                    );
-                }
-            };
-        play_consumer(NoteConsumer(unit), self.interval, 1, play_subpattern);
+        windows.for_each(|interval| {
+            let scaled_start = interval.start().add(self.offset);
+            player.schedule_note_unit(unit.clone(), scaled_start);
+        })
     }
 
     fn map_silence(&self) -> Self::PatternOutput {}
--- src/ast/time/mod.rs
+++ src/ast/time/mod.rs
@@ -1,30 +1,105 @@
 use fixed::FixedI32;
-use fixed::Wrapping;
 use fixed::types::extra::U12;
+
+type Inner = FixedI32<U12>;
 
 /// Used to represent cycles, units of time (progression through a
 /// [crate::ast::pattern::Pattern]) that are not dependent on the number of
 /// beats per minute.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct CycleTime(pub Wrapping<FixedI32<U12>>);
+pub struct CycleTime(Inner);
 
 impl CycleTime {
-    pub const ZERO: Self = Self(Wrapping(FixedI32::ZERO));
+    pub const ZERO: Self = Self(FixedI32::ZERO);
     pub const ONE: Self = Self::from_int(1);
 
+    #[inline]
     pub const fn from_int(time: i32) -> Self {
-        Self(Wrapping(FixedI32::const_from_int(time)))
+        Self(FixedI32::const_from_int(time))
     }
 
+    #[inline]
+    pub const fn to_int(self) -> i32 {
+        self.0.int().to_bits() >> Inner::FRAC_NBITS
+    }
+
+    #[inline]
     pub const fn from_int_recip(time: i32) -> Self {
-        Self(Wrapping(Self::from_int(time).0.0.recip()))
+        Self::from_int(time).recip()
     }
 
-    pub const fn const_add(self, other: Self) -> Self {
-        Self(Wrapping(self.0.0.wrapping_add(other.0.0)))
+    #[inline]
+    #[must_use]
+    pub const fn add(self, other: Self) -> Self {
+        Self(self.0.saturating_add(other.0))
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn sub(self, other: Self) -> Self {
+        Self(self.0.saturating_sub(other.0))
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn mul(self, other: Self) -> Self {
+        Self(self.0.saturating_mul(other.0))
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn div(self, other: Self) -> Self {
+        Self(self.0.saturating_div(other.0))
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn div_euclid(self, other: Self) -> Self {
+        Self(self.0.saturating_div_euclid(other.0))
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn rem_euclid(self, other: Self) -> Self {
+        Self(self.0.rem_euclid(other.0))
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn floor(self) -> Self {
+        Self(self.0.saturating_floor())
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn ceil(self) -> Self {
+        Self(self.0.saturating_ceil())
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn recip(self) -> Self {
+        Self(self.0.saturating_recip())
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn round_up_to_nearest(self, increment: Self) -> Self {
+        self.div(increment)
+            .ceil()
+            .mul(increment)
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn round_down_to_nearest(self, increment: Self) -> Self {
+        self.div(increment)
+            .floor()
+            .mul(increment)
     }
 }
 
+#[allow(unused)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct CycleTimeInterval {
     start: CycleTime,
