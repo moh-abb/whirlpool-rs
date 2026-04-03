b681471325a7e85fa1d7a500c09656d121f3aa71
Mohammad A <ma3523@imperial.ac.uk>
2026-04-03 18:15:32 +0100

    feat: Includes assignment operators (`AddAssign` etc.) for `CycleTime`.

--- src/ast/time/traits.rs
+++ src/ast/time/traits.rs
@@ -1,11 +1,15 @@
 #![deny(unconditional_recursion)]
 
 use core::ops::Add;
+use core::ops::AddAssign;
 use core::ops::Div;
+use core::ops::DivAssign;
 use core::ops::Mul;
+use core::ops::MulAssign;
 use core::ops::Neg;
 use core::ops::Rem;
 use core::ops::Sub;
+use core::ops::SubAssign;
 
 use crate::ast::time::CycleTime;
 
@@ -31,9 +35,23 @@ macro_rules! impl_binary_op {
     };
 }
 
+macro_rules! impl_op_assign {
+    ($tr:ty, $f1: ident, $f2: ident) => {
+        impl $tr for CycleTime {
+            fn $f1(&mut self, rhs: Self) {
+                *self = CycleTime::$f2(*self, rhs)
+            }
+        }
+    };
+}
+
 impl_binary_op!(Add, add, add);
 impl_binary_op!(Sub, sub, sub);
 impl_binary_op!(Mul, mul, mul);
 impl_binary_op!(Div, div, div);
 impl_binary_op!(Rem, rem, rem_euclid);
 impl_unary_op!(Neg, neg, neg);
+impl_op_assign!(AddAssign, add_assign, add);
+impl_op_assign!(SubAssign, sub_assign, add);
+impl_op_assign!(MulAssign, mul_assign, add);
+impl_op_assign!(DivAssign, div_assign, add);
