cb8a82f81f687d110f5628b5710a0c807ae91720
Mohammad A <ma3523@imperial.ac.uk>
2026-04-05 20:07:41 +0100

    feat: Removes `head` from `NoteSequence` and adds `interval`.

--- src/test/interpreter/sequence.rs
+++ src/test/interpreter/sequence.rs
@@ -1,14 +1,13 @@
 use crate::alloc_types::Vec;
-use crate::ast::pattern::Pattern;
 use crate::ast::time::CycleTime;
-use crate::structures::index::Index;
+use crate::ast::time::CycleTimeInterval;
 use crate::test::interpreter::ScheduledExpectation;
 
 #[allow(unused)]
 #[derive(Debug)]
 pub struct NoteSequence {
-    pub head: Index<Pattern>,
+    pub interval: CycleTimeInterval,
     pub offset: CycleTime,
     pub multiplier: CycleTime,
-    pub expected: Vec<(CycleTime, Vec<ScheduledExpectation>)>,
+    pub expected: Vec<ScheduledExpectation>,
 }
