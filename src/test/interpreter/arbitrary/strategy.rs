dd197c8a2ed437ee926d46c8d5c28edf1210e75a
Mohammad A <ma3523@imperial.ac.uk>
2026-04-06 03:32:53 +0100

    test: Uses arbitrary interval instead of end time for testing note sequence.

--- src/test/interpreter/arbitrary/strategy.rs
+++ src/test/interpreter/arbitrary/strategy.rs
@@ -2,10 +2,8 @@ use proptest::prelude::Strategy;
 
 use crate::ast::pattern::Pattern;
 use crate::ast::pattern::arenas::PatternArenas;
-use crate::ast::time::CycleTime;
-use crate::ast::time::CycleTimeInterval;
 use crate::structures::index::Index;
-use crate::test::interpreter::arbitrary::expectations::arb_end_offset_and_multiplier;
+use crate::test::interpreter::arbitrary::expectations::arb_interval_offset_and_multiplier;
 use crate::test::interpreter::arbitrary::expectations::pattern_expectations;
 use crate::test::interpreter::sequence::NoteSequence;
 use crate::test::pattern::arbitrary::arenas_to::ArenasTo;
@@ -19,14 +17,14 @@ impl StrategyWithArena<(Index<Pattern>, NoteSequence)>
     fn item_strategy<Arenas: PatternArenas + 'static>()
     -> impl Strategy<Value = ArenasTo<Arenas, (Index<Pattern>, NoteSequence)>>
     {
-        (arb_small_pattern(), arb_end_offset_and_multiplier()).prop_map(
-            |(arenas_to_pattern, (end_time, offset, multiplier))| {
+        (arb_small_pattern(), arb_interval_offset_and_multiplier()).prop_map(
+            |(arenas_to_pattern, (interval, offset, multiplier))| {
                 ArenasTo::new(move |arenas: &Arenas| {
                     let pattern = arenas_to_pattern.call(arenas)?;
                     let expectations = pattern_expectations(
                         pattern.clone(),
                         arenas,
-                        CycleTimeInterval::new(CycleTime::ZERO, end_time),
+                        interval,
                         offset,
                         multiplier,
                     )?;
