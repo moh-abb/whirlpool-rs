29b2665d709a8efe5b27bbbf5b4467620fff68bd
Mohammad A <ma3523@imperial.ac.uk>
2026-08-09 00:23:13 +0100

    feat: Expands `ArenaError` to include more specific cases when visiting `Linked` structures.
    We could have also tried creating a nw `LinkedError`, but that may be
    too cumbersome.

--- src/mem/arena/error.rs
+++ src/mem/arena/error.rs
@@ -21,14 +21,34 @@ const EXPECTED_FULL_SLOT: &str =
 /// borrowed twice.
 const INVALID_BORROW: &str =
     "[Arena]: Attempted to borrow arena violating borrowing rules";
+/// The message to use when attempting to access an element which is expected
+/// to be linked, and have a parent, but none was found.
+const EXPECTED_PARENT: &str = "[Arena]: Expected parent for the given index";
+/// The message to use when attempting to access an element which is expected
+/// to be linked, and have children, but none were found.
+const EXPECTED_CHILDREN: &str =
+    "[Arena]: Expected children for the given index";
+/// The message to use when attempting to access an element which is expected
+/// to be linked, and have a sibling on a given end, but it was not found.
+const EXPECTED_SIBLING: &str =
+    "[Arena]: Expected a sibling for the given index";
+/// The message to use when an invariant during traversal has been broken.
+/// This should not be used as the main source of error information; it only
+/// serves as a placeholder for a situation that is logically unreachable and
+/// should be complemented by more verbose error propagation.
+const INVARIANT_BROKEN: &str = "[Arena]: Invariant broken during traversal";
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ArenaError {
     IndexOutOfBounds,
     LimitReached,
     ExpectedFreeSlot,
     ExpectedFullSlot,
     InvalidBorrow,
+    ExpectedParent,
+    ExpectedChildren,
+    ExpectedSibling,
+    InvariantBroken,
 }
 
 pub type ArenaResult<T> = Result<T, ArenaError>;
@@ -41,6 +61,10 @@ impl Display for ArenaError {
             Self::ExpectedFreeSlot => EXPECTED_FREE_SLOT,
             Self::ExpectedFullSlot => EXPECTED_FULL_SLOT,
             Self::InvalidBorrow => INVALID_BORROW,
+            Self::ExpectedParent => EXPECTED_PARENT,
+            Self::ExpectedChildren => EXPECTED_CHILDREN,
+            Self::ExpectedSibling => EXPECTED_SIBLING,
+            Self::InvariantBroken => INVARIANT_BROKEN,
         };
         f.write_str(msg)
     }
