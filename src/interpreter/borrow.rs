648a4d6d522650c0d7461986eae74750bd539c00
Mohammad A <ma3523@imperial.ac.uk>
2026-08-09 00:08:45 +0100

    fix: Modifies `BorrowAdapter` to be fallible, to avoid potential runtime panics.

--- src/interpreter/borrow.rs
+++ src/interpreter/borrow.rs
@@ -1,27 +1,32 @@
 //! Used to represent types that can be borrowed either at zero cost
 //! (for mutable references) or (for a [RefCell]) with a borrow guard.
+//! To avoid panics caused by `RefCell`, borrows are fallible and enclosed with
+//! an [ArenaResult].
 
 use core::cell::RefCell;
 use core::ops::DerefMut;
+
+use crate::mem::ArenaError;
+use crate::mem::ArenaResult;
 
 mod private {
     pub trait Sealed {}
 }
 
 pub trait BorrowAdapter<T>: private::Sealed {
-    fn borrow_mut(&mut self) -> impl DerefMut<Target = T>;
+    fn try_borrow_mut(&mut self) -> ArenaResult<impl DerefMut<Target = T>>;
 }
 
 impl<T> private::Sealed for &RefCell<T> {}
 impl<T> BorrowAdapter<T> for &RefCell<T> {
-    fn borrow_mut(&mut self) -> impl DerefMut<Target = T> {
-        RefCell::borrow_mut(self)
+    fn try_borrow_mut(&mut self) -> ArenaResult<impl DerefMut<Target = T>> {
+        RefCell::try_borrow_mut(self).map_err(|_| ArenaError::InvalidBorrow)
     }
 }
 
 impl<T> private::Sealed for &mut T {}
 impl<T> BorrowAdapter<T> for &mut T {
-    fn borrow_mut(&mut self) -> impl DerefMut<Target = T> {
-        self.deref_mut()
+    fn try_borrow_mut(&mut self) -> ArenaResult<impl DerefMut<Target = T>> {
+        Ok(self.deref_mut())
     }
 }
