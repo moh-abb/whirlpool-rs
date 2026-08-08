2843a1a18ee828ac6c976c4e3c91d20e38f01454
Mohammad A <ma3523@imperial.ac.uk>
2026-08-09 00:06:47 +0100

    feat: Implements check to verify whether `Linked` structure is acyclic.

---
+++ src/test/mem/linked.rs
@@ -0,0 +1,64 @@
+use core::fmt::Debug;
+use std::collections::BTreeSet;
+
+use crate::mem::ArenaError;
+use crate::mem::ArenaItem;
+use crate::mem::Index;
+use crate::mem::linked::Linked;
+use crate::mem::linked::TraversalState;
+use crate::mem::linked::VisitRef;
+use crate::mem::linked::visit_linked;
+
+struct CycleFinderState<Node> {
+    visited: BTreeSet<Node>,
+}
+
+impl<Node> TraversalState<Node> for CycleFinderState<Node>
+where
+    Node: ArenaItem + Clone + Debug,
+{
+    type Visit = VisitRef;
+    type Output = ();
+    type Error = ArenaError;
+
+    fn enter_node(
+        &mut self,
+        cur_node: &Node,
+    ) -> Result<Self::Output, Self::Error> {
+        println!("Entering node {cur_node:?}");
+        if self.visited.contains(cur_node) {
+            panic!(
+                "Node has already been visited, all visited nodes: {:?}",
+                &self.visited
+            );
+        }
+        self.visited.insert(cur_node.clone());
+        Ok(())
+    }
+
+    fn exit_node(
+        &mut self,
+        cur_node: &Node,
+    ) -> Result<Self::Output, Self::Error> {
+        println!("Exiting node {cur_node:?}");
+        Ok(())
+    }
+
+    fn post_enter(&mut self, index: Index<Node>) -> Result<(), Self::Error> {
+        println!("Entered {index:?}");
+        Ok(())
+    }
+
+    fn post_exit(&mut self, index: Index<Node>) -> Result<(), Self::Error> {
+        println!("Exited {index:?}");
+        Ok(())
+    }
+}
+
+pub fn check_acyclic<Node, Arenas>(start_index: Index<Node>, arenas: &Arenas)
+where
+    Node: Linked<Node, Arenas> + Clone + Debug,
+{
+    let mut state = CycleFinderState { visited: BTreeSet::new() };
+    visit_linked(start_index, &mut state, arenas).unwrap();
+}
--- src/test/mem/mod.rs
+++ src/test/mem/mod.rs
@@ -1,2 +1,3 @@
 pub mod arena_test;
 pub mod arenas_to;
+pub mod linked;
