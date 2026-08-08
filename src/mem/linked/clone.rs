d289cf6d9e6270cd095108e88e8f2677346f2065
Mohammad A <ma3523@imperial.ac.uk>
2026-08-08 23:58:12 +0100

    feat: Reimplements cloning of generic `Linked` data structures with new function `clone_linked`.

---
+++ src/mem/linked/clone.rs
@@ -0,0 +1,163 @@
+use crate::mem::Arena;
+use crate::mem::ArenaError;
+use crate::mem::ArenaResult;
+use crate::mem::Chain;
+use crate::mem::Cow;
+use crate::mem::Index;
+use crate::mem::Multiple;
+use crate::mem::linked::Linked;
+use crate::mem::linked::drop_linked;
+use crate::mem::linked::traversal_state::TraversalState;
+use crate::mem::linked::visit_linked;
+use crate::mem::linked::visit_type::VisitRef;
+
+/// Used to implement a FSM to represent the current state of cloning as we
+/// traverse the source structure.
+/// In the normal case, the [CloneHandler] alternates between `Ready` and
+/// `Entered`:
+/// - Before entering the first source node, the state is `Ready` with an
+/// empty parent.
+/// - When entering a source node, the node is copied to `src_node` and the
+/// destination parent is recorded.
+/// - On `post_enter`, we attempt to allocate the new node and link it to the
+/// parent. This needs to happen in `post_enter` otherwise we may attempt to
+/// modify the [Arena] while it is already borrowed.
+///     - If allocation is successful, then on entering a child node, the
+/// index that was just allocated will become the new `dest_parent`.
+///     - If allocation failed, then the error is recorded in `Failed`.
+/// - On `post_exit`, we update the parent node to be the parent of the current
+/// parent (assuming it still exists, which should be the case).
+struct CloneHandler<'a, Node, ArenasY, CloneF> {
+    dest_arenas: &'a ArenasY,
+    copied_node: Option<Node>,
+    dest_parent: Option<Index<Node>>,
+    dest_root: Option<Index<Node>>,
+    clone_handler: CloneF,
+}
+
+impl<Node, ArenasY, CloneF> TraversalState<Node>
+    for CloneHandler<'_, Node, ArenasY, CloneF>
+where
+    Node: Linked<Node, ArenasY>,
+    CloneF: FnMut(&Node) -> Node,
+{
+    type Visit = VisitRef;
+    type Output = ();
+    type Error = ArenaError;
+
+    fn enter_node(
+        &mut self,
+        cur_node: &Node,
+    ) -> Result<Self::Output, Self::Error> {
+        debug_assert!(self.copied_node.is_none());
+
+        let mut copied_node = (self.clone_handler)(cur_node);
+        // `Multiple` will allocate and push back the element.
+        // To prepare, clear the copied node's parent and sibling
+        // indices.
+        copied_node.get_mut_parent().take();
+        *copied_node.get_mut_sibling_chain() = Chain::new();
+        if let Some(multiple) = Node::get_mut_children(&mut copied_node) {
+            *multiple = Multiple::new();
+        }
+
+        self.copied_node = Some(copied_node);
+        Ok(())
+    }
+
+    fn exit_node(&mut self, _: &Node) -> Result<Self::Output, Self::Error> {
+        // Indicate true to leave successfully; we never repeat iteration.
+        Ok(())
+    }
+
+    fn post_enter(&mut self, _: Index<Node>) -> Result<(), Self::Error> {
+        // Take the node we got when we were still inside the arena slot.
+        let copied_node = self
+            .copied_node
+            .take()
+            .ok_or(ArenaError::InvariantBroken)?;
+
+        // Allocate the child node, this will be the "next parent" when
+        // traversing downwards
+        let next_parent = if let Some(parent) = self.dest_parent.take() {
+            let new_node_index = Multiple::push_back(
+                self.dest_arenas,
+                parent,
+                Cow::Owned(copied_node),
+            )?;
+            // We might enter the index's children: update the parent
+            new_node_index
+        } else {
+            // Just allocate the root node on its own because there cannot
+            // be any sibling nodes that need to be modified.
+            let arena = Node::child_arena(self.dest_arenas);
+            let new_node_index = arena.push(copied_node)?;
+
+            // Don't forget to store the root node as well.
+            self.dest_root = Some(new_node_index.clone());
+
+            new_node_index
+        };
+
+        self.dest_parent = Some(next_parent);
+        Ok(())
+    }
+
+    fn post_exit(&mut self, _: Index<Node>) -> Result<(), Self::Error> {
+        // We are moving back up, so update the node to be the parent
+        let Some(parent) = self.dest_parent.take() else {
+            return Err(ArenaError::ExpectedParent);
+        };
+
+        let arena = Node::child_arena(self.dest_arenas);
+        self.dest_parent =
+            arena.map(parent, |node| node.get_parent().clone())?;
+
+        Ok(())
+    }
+}
+
+/// Clones a linked data structure from arena X into arena Y, given a function
+/// that clones each element.
+/// On success, the root node will not have a parent even if the original
+/// element at `start_index` did.
+///
+/// The source structure will be traversed, and the clone function will be
+/// called on each element. Then the cloned element has its source parent and
+/// siblings removed and replaced with the destination parent and siblings
+/// in the new arena.
+///
+/// Because allocation can fail once the arena reaches capacity, the result
+/// may need to be dropped (in which case `drop_handler` will be called on
+/// each element and the partial result will be removed).
+pub fn clone_linked<Node, Arenas>(
+    start_index: Index<Node>,
+    source_arenas: &Arenas,
+    dest_arenas: &Arenas,
+    clone_handler: impl FnMut(&Node) -> Node,
+) -> ArenaResult<Index<Node>>
+where
+    Node: Linked<Node, Arenas>,
+{
+    let mut state = CloneHandler {
+        dest_arenas,
+        copied_node: None,
+        dest_parent: None,
+        dest_root: None,
+        clone_handler,
+    };
+    let visit_result = visit_linked(start_index, &mut state, source_arenas);
+
+    if let Err(err) = visit_result {
+        if let Some(root) = state.dest_root {
+            drop_linked(root, dest_arenas)?;
+        }
+        return Err(err);
+    }
+
+    // Entering the node for the first time should change the clone state to
+    // have a destination root
+    state
+        .dest_root
+        .ok_or(ArenaError::InvariantBroken)
+}
--- src/mem/linked/mod.rs
+++ src/mem/linked/mod.rs
@@ -4,6 +4,7 @@ use crate::mem::Chain;
 use crate::mem::Index;
 use crate::mem::Multiple;
 
+pub mod clone;
 pub mod cmp;
 pub mod drop;
 pub mod traversal_state;
