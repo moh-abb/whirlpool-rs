0ce07d52ee128b693694582482334e7882972c13
Mohammad A <ma3523@imperial.ac.uk>
2026-08-08 23:44:17 +0100

    refactor: Simplifies arena allocation of `Pattern` to use "flat" nodes instead of mutual recursion with `TimedStep`.
    Note that this comes at the cost of avoiding some type safety since now
    there are no guarantees that a `TimedStep` is always and only the child
    of a `TimeCat` or `Arrange`, or for other cases with patterns of
    different numbers of arguments. Will need to make tests that maintain
    invariants like this later.

--- src/ast/pattern/arenas.rs
+++ src/ast/pattern/arenas.rs
@@ -1,13 +1,8 @@
 use core::fmt::Debug;
 
-use crate::ast::Pattern;
-use crate::ast::TimedStep;
+use crate::ast::PatternNode;
 use crate::mem::Arena;
-use crate::mem::Chain;
 
 pub trait PatternArenas: Debug {
-    fn get_pattern_arena(&self) -> &impl Arena<Pattern>;
-    fn get_pattern_chain_arena(&self) -> &impl Arena<Chain<Pattern>>;
-    fn get_timed_step_arena(&self) -> &impl Arena<TimedStep>;
-    fn get_timed_step_chain_arena(&self) -> &impl Arena<Chain<TimedStep>>;
+    fn get_pattern_arena(&self) -> &impl Arena<PatternNode>;
 }
