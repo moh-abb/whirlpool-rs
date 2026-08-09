2a80cdd8a1b24c3635b6c5275f5f78495bd69801
Mohammad A <ma3523@imperial.ac.uk>
2026-08-09 02:54:34 +0100

    refactor: Replaces generic `Error` in `EvaluateFrame` with `PatternInterpreterError`.

--- src/interpreter/frame.rs
+++ src/interpreter/frame.rs
@@ -1,6 +1,17 @@
 use core::ops::ControlFlow;
 
-pub enum InterpreterFrame {}
+use crate::ast::Pattern;
+use crate::ast::PatternNode;
+use crate::ast::pattern::arenas::PatternArenas;
+use crate::interpreter::error::PatternInterpreterResult;
+use crate::interpreter::props::PlayElemArgs;
+use crate::mem::Arena;
+use crate::mem::Index;
+use crate::synth::scheduler::UnitScheduler;
+
+pub enum InterpreterFrame<'a, Arenas> {
+    Query(QueryFrame),
+}
 
 /// Indicates the result of interpreting one frame.
 /// If we return `Err(err)` then this indicates that interpreting has failed
@@ -12,11 +23,43 @@ pub type InterpreterResult<'a, Arenas> = PatternInterpreterResult<
     ControlFlow<(), Option<InterpreterFrame<'a, Arenas>>>,
 >;
 
-pub trait EvaluateFrame {
-    type Error;
-
+pub trait EvaluateFrame<'a, Arenas> {
     fn step(
         &mut self,
         scheduler: &mut impl UnitScheduler,
-    ) -> EvaluateResult<Self::Error>;
+        arenas: &'a Arenas,
+    ) -> InterpreterResult<'a, Arenas>;
+}
+
+pub struct QueryFrame {
+    play_args: PlayElemArgs<Index<PatternNode>>,
+}
+
+pub fn query_frame<'a, Arenas>(
+    play_args: PlayElemArgs<Index<PatternNode>>,
+) -> InterpreterFrame<'a, Arenas> {
+    InterpreterFrame::Query(QueryFrame { play_args })
+}
+
+impl<'a, Arenas: PatternArenas> EvaluateFrame<'a, Arenas> for QueryFrame {
+    fn step(
+        &mut self,
+        _: &mut impl UnitScheduler,
+        arenas: &'a Arenas,
+    ) -> InterpreterResult<'a, Arenas> {
+        let index = self.play_args.elem.clone();
+        let cloned_node = arenas
+            .get_pattern_arena()
+            .map(index, Clone::clone)?;
+        let opt_next_frame = match &cloned_node.pattern {
+            Pattern::Cat(multiple) => todo!(),
+            Pattern::Seq(multiple) => todo!(),
+            Pattern::Stack(multiple) => todo!(),
+            Pattern::TimeCat { total_cycle_length, multiple } => todo!(),
+            Pattern::Arrange { total_cycle_length, multiple } => todo!(),
+            Pattern::TimedStep(timed_step) => todo!(),
+            Pattern::Note(note_unit) => todo!(),
+            Pattern::Silence => todo!(),
+        };
+    }
 }
