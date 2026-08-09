a41d1745100ce56f9b133a2cc107c9d3bb76d12f
Mohammad A <ma3523@imperial.ac.uk>
2026-08-09 02:58:10 +0100

    feat: Adds new cases of `PatternInterpreterError` for typical errors (empty `Multiple`, overflow and broken structures).

--- src/interpreter/error.rs
+++ src/interpreter/error.rs
@@ -2,10 +2,13 @@ use crate::ast::time::OverflowError;
 use crate::mem::ArenaError;
 
 /// Represents the error type when interpreting a [Pattern].
-#[derive(derive_more::From, Clone)]
+#[derive(derive_more::From, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum PatternInterpreterError {
     ArenaErr(ArenaError),
-    OverflowErr(OverflowError),
+    Overflow(OverflowError),
+    MultipleEmpty,
+    ExpectedTimedStep,
+    ExpectedNormalPattern,
 }
 
 pub type PatternInterpreterResult<T> = Result<T, PatternInterpreterError>;
